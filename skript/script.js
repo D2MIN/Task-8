f3c846c6d559d90022c1ad0f278c440fbc73255a
D2MIN <aamor8448@gmail.com>
2023-11-15 22:08:11 +0300

    Change 2

--- skript/script.js
+++ skript/script.js
@@ -1,16 +1,29 @@
-let input = document.getElementById("btn_inp");
-let output = document.getElementById("btn_out");
+let btn_form = document.getElementById("btn_form");
 let close = document.getElementById("popup_close");
 let popup = document.getElementById("popup");
+let popup_content = document.getElementById('popup_content');
 
+let popup_content_flag = false;
 
-
-input.addEventListener('click', ()=>{
+btn_form.addEventListener('click', () => {
     popup.style.visibility = "visible";
     popup.style.opacity = 1;
 })
 
-close.addEventListener('click', ()=>{
+close.addEventListener('click', () => {
     popup.style.visibility = "hidden";
     popup.style.opacity = 0;
+})
+
+popup.addEventListener('click', () => {
+    if (popup_content_flag != true) {
+        popup.style.visibility = "hidden";
+        popup.style.opacity = 0;
+    } else {
+        popup_content_flag = false
+    }
+})
+
+popup_content.addEventListener('click', () => {
+    popup_content_flag = true;
 })
